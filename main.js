40be2865e59724076009d2e7ac6da6b0a20f40d5
hroque01 <hadson.roque01@gmail.com>
2022-11-21 13:43:45 +0100

    Milestone 0

---
+++ main.js
@@ -0,0 +1,60 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+//persone
+//Wayne Barnett - Founder & CEO
+//Angela Caroll - Chief Editor
+//Walter Gordon - Office Manager
+//Angela Lopez - Social Media Manager
+//Scott Estrada - Developer
+//Barbara Ramos - Graphic Designer
+
+const personale = [
+    {
+        "nome" : 'Wayne Barnett',
+        "Ruolo" : 'Founder & CEO'
+    },
+
+    {
+        "nome" : 'Angela Caroll',
+        "Ruolo" : 'Chief Editor'
+    },
+
+    {
+        "nome" : 'Walter Gordon',
+        "Ruolo" : 'Office Manager'
+    },
+
+    {
+        "nome" : 'Angela Lopez',
+        "Ruolo" : 'Social Media Manager'
+    },
+    
+    {
+        "nome" : 'Scott Estrada',
+        "Ruolo" : 'Developer'
+    },
+
+    {
+        "nome" : 'Barbara Ramos',
+        "Ruolo" : 'Graphic Designer'
+    }
+
+]
+
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
+// BONUS 1:
+// Trasformare la stringa foto in una immagine effettiva
+// BONUS 2:
+// Organizzare i singoli membri in card/schede
+
+
